a7dc3e2234d9ef7c00b1d544576b23cabafb7eab
Victor Storozhenko <storozvic@gmail.com>
2019-12-10 20:12:31 -0800

    app.js entry only

--- app.js
+++ app.js
@@ -40,5 +40,35 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500)
   res.render('error')
 })
+const PORT = process.env.PORT || '3000'
 
-module.exports = app
+app.listen(PORT, err => {
+  if (err) return onError(err)
+  console.log(`Server listen on ${PORT}`)
+})
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error
+  }
+
+  var bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges')
+      process.exit(1)
+      break
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use')
+      process.exit(1)
+      break
+    default:
+      throw error
+  }
+}
